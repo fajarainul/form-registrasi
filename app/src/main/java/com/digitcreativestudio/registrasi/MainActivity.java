287cff9429129a5776eab84cdc129dee8807f287
Faqih Arifian <faqiharifianaji@gmail.com>
2017-03-23 14:29:26 +0700

    tambah id dan inisialisasi ke main activity

--- app/src/main/java/com/digitcreativestudio/registrasi/MainActivity.java
+++ app/src/main/java/com/digitcreativestudio/registrasi/MainActivity.java
@@ -1,13 +1,48 @@
 package com.digitcreativestudio.registrasi;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.EditText;
+import android.widget.Spinner;
 
 public class MainActivity extends AppCompatActivity {
+    Spinner idTypeSpinner, provinceSpinner, regencySpinner, districtSpinner, villageSpinner;
+    EditText idEditText, nameEditText, phoneEditText, addressEditText;
 
+    Spinner companyProvinceSpinner, companyRegencySpinner, companyDistrictSpinner, companyVillageSpinner;
+    EditText companyNpwpEditText, companyNoEditText, companyNameEditText, companyAddressEditText, companyPhoneEditText;
+
+    Spinner licenseSpinner, licenseRegionSpinner;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        idEditText = (EditText) findViewById(R.id.id_edittext);
+        nameEditText = (EditText) findViewById(R.id.name_edittext);
+        phoneEditText = (EditText) findViewById(R.id.phone_edittext);
+        addressEditText = (EditText) findViewById(R.id.address_edittext);
+
+        idTypeSpinner = (Spinner) findViewById(R.id.id_type_spinner);
+        provinceSpinner = (Spinner) findViewById(R.id.province_spinner);
+        regencySpinner = (Spinner) findViewById(R.id.regency_spinner);
+        districtSpinner = (Spinner) findViewById(R.id.district_spinner);
+        villageSpinner = (Spinner) findViewById(R.id.village_spinner);
+
+        companyNpwpEditText = (EditText) findViewById(R.id.company_npwp_edittext);
+        companyNoEditText = (EditText) findViewById(R.id.company_no_edittext);
+        companyNameEditText = (EditText) findViewById(R.id.company_name_edittext);
+        companyAddressEditText = (EditText) findViewById(R.id.company_address_edittext);
+        companyPhoneEditText = (EditText) findViewById(R.id.company_phone_edittext);
+
+        companyProvinceSpinner = (Spinner) findViewById(R.id.company_province_spinner);
+        companyRegencySpinner = (Spinner) findViewById(R.id.company_regency_spinner);
+        companyDistrictSpinner = (Spinner) findViewById(R.id.company_district_spinner);
+        companyVillageSpinner = (Spinner) findViewById(R.id.company_village_spinner);
+
+        licenseSpinner = (Spinner) findViewById(R.id.license_spinner);
+        licenseRegionSpinner = (Spinner) findViewById(R.id.license_region_spinner);
+
+
     }
 }
